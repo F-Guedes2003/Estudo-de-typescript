77b610b0ea69682fb3a0963b32bf76a1c532520c
F-Guedes2003 <fhelippedamaspertar21@gmail.com>
2024-04-08 16:05:11 -0300

    Exercicio de conversão de celsius para fahrenheit adicionado

---
+++ Introdução/exercicios/Conversor de temperatura/scripts.js
@@ -0,0 +1,4 @@
+var temp = 22;
+var newTemp;
+newTemp = temp * 1.8 + 32;
+console.log(newTemp);
---
+++ Introdução/exercicios/Conversor de temperatura/scripts.ts
@@ -0,0 +1,6 @@
+let temp: number = 22;
+let newTemp: number;
+
+newTemp = temp * 1.8 + 32;
+
+console.log(newTemp);
