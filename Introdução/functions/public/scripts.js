cbc109a5ea8d1d928131314832e01fab9de1df23
F-Guedes2003 <fhelippedamaspertar21@gmail.com>
2024-04-11 12:00:03 -0300

    type aliases folder

--- Introdução/functions/public/scripts.js
+++ Introdução/functions/public/scripts.js
@@ -1,4 +1,9 @@
 "use strict";
-let sayHello;
-sayHello = () => console.log("hello");
+let sayHello; //We can declare that sayHello will be a function
+sayHello = () => console.log("Hello");
+// let add = (num1: number, num2: number) => num1 + num2; //We can specify the types of the parameters that it will be received
+let add = (num1, num2, num3) => {
+    console.log(num1 + num2);
+};
+add(3, 5);
 sayHello();
--- Introdução/functions/src/scripts.ts
+++ Introdução/functions/src/scripts.ts
@@ -1,3 +1,13 @@
-function sayHello(){
-    console.log("Hello");
-}
+let sayHello: Function; //We can declare that sayHello will be a function
+sayHello = () => console.log("Hello"); 
+
+// let add = (num1: number, num2: number) => num1 + num2; //We can specify the types of the parameters that it will be received
+
+let add = (num1:number, num2: number, num3?: number) => { //we can set an optional value using the "?" after it name
+    console.log(num1 + num2);
+};
+
+
+
+add(3, 5);
+sayHello();
